1d8c9e69469c21a136a14871fd5dc5b0ad115502
Matías Hermosilla <matiasahermosilla@gmail.com>
2018-03-23 21:17:24 -0300

    Eliminando código inutilizado

--- src/java/security/UserService.java
+++
@@ -1,9 +0,0 @@
-package security;
-
-import org.springframework.security.authentication.AccountStatusUserDetailsChecker;
-
-public class UserService {
-    
-    private final AccountStatusUserDetailsChecker detailsChecker = new AccountStatusUserDetailsChecker();
-    
-}
